e66a01a976e3ade7d3530cd2fd99271ec6f9417c
unknown <you@example.com>
2023-10-12 17:08:11 -0600

    Mi primer commit

---
+++ Assets/js/main.js
@@ -0,0 +1,8 @@
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let hamburger = document.querySelector('#hamburger');
+    hamburger.addEventListener('click', () => {
+        navbar.classList.toggle('open');
+    });
+})()
+
